0445de3cad510af219824c32678760ae83d19af4
Ben Capistrant <73549762+bkcapistrant@users.noreply.github.com>
2026-01-28 16:13:09 -0500

    Uploading main script, example usage notebook, and README

---
+++ README.md
@@ -0,0 +1,84 @@
+# Transit Visualizer 
+
+A Python toolkit for planning and visualizing exoplanet transit observations from ground-based observatories.
+
+## Overview
+
+`TransitVisual.py` provides comprehensive functionality for visualizing the observability of transiting exoplanet events from Earth-based observatories. The script integrates the astronomical calculations and observability constraints calculated by the [`astroplan` Open-Source Observation Planning Package in Python](https://ui.adsabs.harvard.edu/abs/2018AJ....155..128M/abstract), providing additional data visualization functions to help observers plan effective, or simultaneous, ground-based transit observations. The visualizations and information displayed for transiting events are inspired by the [TAPIR](https://astro.swarthmore.edu/~jensen/tapir.html#fn1) web interface for planning astronomical observations, but with outputs displayable in Jupyter Notebooks via html wrapped `Pandas` DataFrames. 
+
+## Usage
+
+`TransitVisualExample.ipynb` is a Jupyter Notebook that shows an example of creating the required inputs for the visualization using [`astroplan`](https://astroplan.readthedocs.io/en/stable/index.html).
+
+The order of this notebook follows the tutorials for creating [`Observer`](https://astroplan.readthedocs.io/en/stable/api/astroplan.Observer.html#astroplan.Observer) objects, followed by defining customizable observational constraints using their `constraints` module. A more complete tutorial for these steps can be found on their [documentation pages](https://astroplan.readthedocs.io/en/stable/tutorials/constraints.html). The example notebook includes both a manually input observatory (Mount Lemon SkyCenter Observatory), and another generated from the list of observatories stored in astroplan (Gran Telescopio CANARIAS; GTC). 
+
+Next parameters for all exoplanet systems are read in from the [`nasa_exoplanet_archive`](https://astroquery.readthedocs.io/en/latest/ipac/nexsci/nasa_exoplanet_archive.html#module-astroquery.ipac.nexsci.nasa_exoplanet_archive) service using [astroquery](https://astroquery.readthedocs.io/). An example target ('TOI-1268 b') is chosen to create an [`EclipsingSystem`](https://astroplan.readthedocs.io/en/stable/api/astroplan.EclipsingSystem.html#astroplan.EclipsingSystem) object, following the [Observing Transiting Exoplanets and Eclipsing Binaries tutorial](https://astroplan.readthedocs.io/en/stable/tutorials/periodic.html), using the exoplanet system parameters queried in the previous cells. 
+
+The [`next_primary_eclipse_time`](https://astroplan.readthedocs.io/en/stable/api/astroplan.EclipsingSystem.html#astroplan.EclipsingSystem.next_primary_eclipse_time) and [`next_primary_ingress_egress_time`](https://astroplan.readthedocs.io/en/stable/api/astroplan.EclipsingSystem.html#astroplan.EclipsingSystem.next_primary_ingress_egress_time) methods are used to calculate the next 50 transits (set using keyword `n_eclipses`) which generates a `astropy` time object list of mid-transit times and a 2-D array of ingress and egress `astropy` times respectively. The eclipses times for the target planet are calculated after the set observation time, which in this example this has been set to 2026 March 1 00:00 UTC,. After creating the [FixedTarget](https://astroplan.readthedocs.io/en/stable/api/astroplan.FixedTarget.html#astroplan.FixedTarget) object for the selected example target, TOI-1268 b, we have all of the inputs required for the `TransitVisual.py` script. 
+
+The first function demonstrated after importing `TransitVisual` is `.display_transits()`, which provides an output dataframe for the earlier specified number of transits (User defined `n_eclipses`) for a given target from a singular observatory. The parameter `plot` in this function is default set to True, which will display the html version of the pandas dataframe with the icon for each transit shown below, which is based on the design of the TAPIR [transit finder charts](https://astro.swarthmore.edu/transits/) 
+
+![](assets/display_transits_example.png)
+
+
+
+`plot` can be set to `False` to return a pandas dataframe that contains the values embeded in this display. 
+
+
+
+The argument `visibility_cut` was set to 90 in this example, to only display transits from the list of 50 that have a transit observability of 90% or higher. If this value is unspecified, the output will include a row for each transit specified by `n_eclipses`.
+
+### _Table column descriptions_
+- The **`Local Evening Date`** column displays the observatory location, as well as the local evening date of the observation and local timezone. 
+
+- The moon illumination and separation displayed in the **`Moon`** column are calculated from the input observatory at mid-transit time for each given transit. 
+
+- The **`Start-Mid-End`** column gives the ingress, mid-transit, and egress times (hour:minute) listed from top to bottom. The **`Alt (S/M/E)`** column list the target altitude at these times in the same order. 
+
+- The **`Transit`** column shows a html icon of the visible portion of the target's transit as a simple box transit model. The blue line corresponds to portions of the transit and baseline that are observable based on the input constraints. 
+
+- The **`% Transit`** column indicates the percentage of the in-transit flux (ingress through egress) that is visible from the observatory location. These are colored based on the visibility with red being low visibility to dark green being 100% visible. 
+
+- **`% Baseline`** indicates the percentage of the out-of-transit flux that is visible from the observatory location. In the notebook example, the observation baseline was set to 1 hour for the desired time to include before and after the ingress and egress times. This baseline keyword defaults to half the input transit duration if left unspecified, similar to the TAPIR inputs. These values are also colored based on visibility percentage similar to the **`% Transit`** column. 
+
+- The **`V mag`** column indicates the stellar V mag provided by the exoplanet archive parameters. 
+
+- The **`Depth (ppt)`** column indicates the transit depth provided by the exoplanet archive parameters (given in relative flux) converted to parts per thousand (ppt) as displayed in the TAPIR outputs. 
+
+- The **`Target`** column displays the target name. 
+
+
+## Multi-Observatory Comparison 
+The `.simultaneous_observing()` function is demonstrated next, which allows you to compare observability across multiple observatories for coordinated campaigns or simultaneous observations.
+
+The inputs are the same as `.display_transits()` but with an additional argument for a second `Observer` object. The output table includes all but the last three columns of the `.display_transits()` output table described above. 
+
+After the **`% Baseline`** column, some of the parameters are shown to display the visibility from the second observer. The column names are the same but differentiated with **`@observatory_2.name`** as shown in the example below comparing GTC at Roque de los Muchachos, La Palma, and the Dharma Endownment Foundation Telescope at the Mount Lemon SkyCenter Observatory:
+
+![](assets/simultaneous_observing_example.png)
+
+An important note here is that the `visibility_cut` argument (still set to 90 in this example)  will be defined based on the in-transit visibility percentage from observatory 1.
+
+<!-- <span style="background-color: #000066; border-radius: 5px;">"Moon"</span>  -->
+
+
+
+## Dependencies
+
+- `astroplan`: Observatory definitions and constraint evaluation
+- `astropy`: Time handling, coordinates, units
+- `astroquery`: NASA Exoplanet Archive access
+- `pandas`: Data organization and presentation
+- `matplotlib`: Plotting and visualization
+- `numpy`: Numerical calculations
+
+## Use Cases
+
+- Planning observation schedules for transit follow-up programs
+- Evaluating feasibility of transit observations from specific sites
+- Coordinating multi-site simultaneous observations
+<!-- - Assessing moon impact on photometric precision -->
+<!-- - Generating observation reports and planning documents -->
+
+## Attribution
+These functions rely almost entirely on the astroplan package, and thus we recommend citing [Morris et al. 2018](https://ui.adsabs.harvard.edu/abs/2018AJ....155..128M/abstract) if you use this for any of your work as requested on their documentation page. 
---
+++ TransitVisual.py
@@ -0,0 +1,411 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.dates as mdates
+from astropy.visualization import astropy_mpl_style, quantity_support
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from IPython.display import HTML, display
+from io import BytesIO
+import base64
+from dataclasses import dataclass
+
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore', np.RankWarning)
+
+from astroplan import Observer, FixedTarget ,Constraint, is_observable, observability_table
+from astroplan import EclipsingSystem
+from astroplan import (AltitudeConstraint, AirmassConstraint,
+                       AtNightConstraint, MoonSeparationConstraint)
+from astroplan.moon import moon_illumination
+from astropy.coordinates import get_moon
+from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
+
+
+def observing_night_from_sunset(observer, time):
+    sunset = observer.sun_set_time(time, which="previous")
+    local_dt = sunset.to_datetime(timezone=observer.timezone)
+    return local_dt.strftime("%a. %Y-%m-%d")
+
+def get_moon_data(observer, target, time):
+    # Get moon illumination (0.0 - 1.0)
+    # 0 = New Moon, 1 = Full Moon
+    illumination = moon_illumination(time)
+    
+    # Get Moon coordinates from the observer's location
+    moon_coord = get_moon(time, location=observer.location)
+    
+    # Calculate angular separation
+    # separation = target.coord.separation(moon_coord)
+    separation = moon_coord.separation(target.coord)
+    
+    return illumination, separation
+
+def alt_at(time,observer,target):
+    return observer.altaz(time, target).alt
+
+def box_transit(t, t_ing, t_egr, depth):
+    y = np.zeros_like(t)
+    in_transit = (t >= t_ing) & (t <= t_egr)
+    y[in_transit] = -depth
+    return y
+
+
+def evaluate_transit(
+    observer,
+    target,
+    ingress,
+    mid,
+    egress,
+    constraints,
+    baseline=None,
+    nsamples=200
+):
+    duration = egress - ingress
+    if baseline is None:
+        baseline = 0.5 * duration
+    else:
+        baseline = baseline*u.hour
+
+    t0 = ingress - baseline
+    t1 = egress + baseline
+
+    times = t0 + np.linspace(
+        0, (t1 - t0).to_value(u.hour), nsamples
+    ) * u.hour
+
+    # Evaluate constraints
+    observable = np.ones(len(times), dtype=bool)
+    for c in constraints:
+        observable &= c(observer, target, times)
+
+    # Fractions
+    in_transit = (times >= ingress) & (times <= egress)
+
+    frac_transit = observable[in_transit].mean()
+    frac_baseline = observable[~in_transit].mean()
+
+    # Moon
+    moon = get_moon(times, observer.location)
+    moon_sep = moon.separation(target.coord).min()
+    moon_illum = moon_illumination(mid)
+
+    # Altitudes
+    def alt(t):
+        return observer.altaz(t, target).alt
+
+    alts = {
+        "start": alt(ingress),
+        "mid": alt(mid),
+        "end": alt(egress),
+    }
+
+    return {
+        "times": times,
+        "observable": observable,
+        "frac_transit": frac_transit,
+        "frac_baseline": frac_baseline,
+        "moon_sep": moon_sep,
+        "moon_illum": moon_illum,
+        "alts": alts,
+    }
+
+def transit_icon(times, ingress, egress, observable, depth):
+    fig, ax = plt.subplots(figsize=(2.2, 0.6))
+
+    y = np.zeros(len(times))
+    in_transit = (times >= ingress) & (times <= egress)
+    y[in_transit] = -depth
+
+    # Non-observable (dashed)
+    ax.plot(times.jd, y, color='0.7', lw=1, ls='--')
+
+    # Observable (blue)
+    ax.plot(times[observable].jd, y[observable], color='C0', lw=2)
+
+    ax.set_axis_off()
+    plt.tight_layout(pad=0.1)
+
+    buf = BytesIO()
+    plt.savefig(buf, format='png', dpi=150)
+    plt.close(fig)
+
+    data = base64.b64encode(buf.getvalue()).decode()
+    return f'<img src="data:image/png;base64,{data}"/>'
+
+@dataclass
+class TransitEvent:
+    target: object
+    ingress: Time
+    mid: Time
+    egress: Time
+    depth: float
+    mag: float
+
+
+def create_transit_events(target, mid_transits, ingress_times, egress_times, params, constraints=None):
+    """
+    Create a list of TransitEvent objects from input parameters.
+    
+    Parameters:
+    -----------
+    target : FixedTarget
+        The target object from astroplan
+    mid_transits : list or array of Time objects
+        Mid-transit times
+    ingress_times : list or array of Time objects
+        Ingress times
+    egress_times : list or array of Time objects
+        Egress times
+    params : dict
+        Dictionary containing 'pl_trandep' (transit depth) and 'sy_vmag' (V magnitude)
+    constraints : list, optional
+        List of observability constraints
+        
+    Returns:
+    --------
+    events : list
+        List of TransitEvent objects
+    """
+    events = []
+    
+    for mid, ing, egr in zip(mid_transits, ingress_times, egress_times):
+        events.append(
+            TransitEvent(
+                target=target,
+                ingress=ing,
+                mid=mid,
+                egress=egr,
+                depth=np.array(params['pl_trandep']),
+                mag=np.array(params['sy_vmag']),
+            )
+        )
+    
+    return events
+
+def display_transits(target, mid_transits, ingress_times, egress_times, params, observer, constraints,baseline=None, visibility_cut=None,plot=True):
+    """
+    Generate a DataFrame summarizing transit observability for multiple events given target and observatory objects from astroplan.
+
+    Parameters:
+    -----------
+    target : FixedTarget
+        The target object from astroplan
+    mid_transits : list or array of Time objects
+        Mid-transit times
+    ingress_times : list or array of Time objects
+        Ingress times
+    egress_times : list or array of Time objects
+        Egress times
+    params : dict
+        Exoplanet archive dictionary containing 'pl_trandep' (transit depth) and 'sy_vmag' (V magnitude)
+    observer : Observeratory object
+        The observatory object from astroplan
+    constraints : list
+        List of observability constraints from astroplan
+    baseline : Astropy Quantity (time), optional
+        Baseline duration around transit for observability calculations
+        defaults to 0.5*transit duration if None
+    visibility_cut : float, optional
+        Percentage of transit visibility to include in output DataFrame
+    plot : bool, optional
+        If True, display the DataFrame as an HTML table. If False, return the DataFrame.
+    Returns:
+    --------
+    df : pandas DataFrame
+        DataFrame summarizing transit observability metrics for each event
+
+    """
+    events = create_transit_events(target, mid_transits, ingress_times, egress_times, params, constraints)
+    
+    rows = []
+    
+    for event in events:
+        metrics = evaluate_transit(
+            observer,
+            event.target,
+            event.ingress,
+            event.mid,
+            event.egress,
+            constraints,
+            baseline=baseline,
+            nsamples=200
+        )
+        
+        icon = transit_icon(
+            metrics["times"],
+            event.ingress,
+            event.egress,
+            metrics["observable"],
+            event.depth
+        )
+        
+        rows.append({
+            "Local Evening Date": f"{observer.name}<br>"
+            f"{observing_night_from_sunset(observer, event.mid)}<br>"
+            f"({observer.timezone})",
+            "Moon": (
+                f"{metrics['moon_illum']:.0%} @ "
+                f"{metrics['moon_sep'].to(u.deg).value:.0f}°"
+            ),
+            "Start-Mid-End": (
+                f"{event.ingress.strftime('%H:%M')}<br>"
+                f"{event.mid.strftime('%H:%M')}<br>"
+                f"{event.egress.strftime('%H:%M')}"
+            ),
+            "Alt (S/M/E)": (
+                f"{metrics['alts']['start'].value:.0f}°<br>"
+                f"{metrics['alts']['mid'].value:.0f}°<br>"
+                f"{metrics['alts']['end'].value:.0f}°"
+            ),
+            "Transit": icon,
+            "% Transit": 100*metrics['frac_transit'],
+            "% Baseline": 100*metrics['frac_baseline'],
+            "V mag": float(event.mag),
+            "Depth (ppt)": float(event.depth)*10,
+            "Target": event.target.name,
+        })
+    
+    
+    df = pd.DataFrame(rows)
+    if visibility_cut is not None:
+        df = df[df["% Transit"] >= visibility_cut]
+
+    if plot:
+        return (df.style
+                .format({'% Transit':'{:.0f}%','% Baseline':'{:.0f}%','Depth (ppt)':'{:.2f}','V mag':'{:.2f}'})
+                .set_properties(**{'background-color':"#FFFFFF",'color':'black'})
+                .set_table_styles([{"selector":"th.col_heading","props":"text-align:center; font-size:1.5em; background-color: #000066;"}])
+                .background_gradient(subset=['% Transit','% Baseline'],cmap="RdYlGn",vmin=0,vmax=100))
+        # return display(HTML(df.style.set_properties(**{'background-color': "#FFFFFF", 'color': 'black'}).to_html(escape=False, index=False)))
+    else:
+        return df
+
+
+def simultaneous_observing(target, mid_transits, ingress_times, egress_times, params, observatory_1,comparison_observatory, constraints, baseline=None, visibility_cut=None,plot=True):
+    """
+    Compare transit observability across multiple observatories for a given target and transit events.
+
+    Parameters:
+    -----------
+    target : FixedTarget
+        The target object from astroplan
+    mid_transits : list or array of Time objects
+        Mid-transit times
+    ingress_times : list or array of Time objects
+        Ingress times
+    egress_times : list or array of Time objects
+        Egress times
+    params : dict
+        Exoplanet archive dictionary containing 'pl_trandep' (transit depth) and 'sy_vmag' (V magnitude)
+    observatory_1 : Observatory object from astroplan
+        First observatory for comparison
+    comparison_observatory : Observatory object from astroplan
+        Second observatory for comparison
+    constraints : list
+        List of observability constraints from astroplan
+    baseline : Astropy Quantity (time), optional
+        Baseline duration around transit for observability calculations 
+        defaults to 0.5*transit duration if None
+    visibility_cut : float, optional
+        Percentage of transit visibility to include in output DataFrame
+    plot : bool, optional
+        Whether to return a html styled DataFrame for plotting (default is True)
+    Returns:
+    --------
+    df : pandas DataFrame
+        DataFrame summarizing transit observability metrics for each event and observatory
+
+    """
+    all_rows = []
+    
+    
+    events = create_transit_events(target, mid_transits, ingress_times, egress_times, params, constraints)
+    
+    for event in events:
+        metrics = evaluate_transit(
+            observatory_1,
+            event.target,
+            event.ingress,
+            event.mid,
+            event.egress,
+            constraints,
+            baseline=baseline,
+            nsamples=200
+        )
+        icon = transit_icon(
+            metrics["times"],
+            event.ingress,
+            event.egress,
+            metrics["observable"],
+            event.depth
+        )
+
+        metrics2 = evaluate_transit(
+            comparison_observatory,
+            event.target,
+            event.ingress,
+            event.mid,
+            event.egress,
+            constraints,
+            baseline=baseline,
+            nsamples=200
+        )
+        icon2 = transit_icon(
+            metrics2["times"],
+            event.ingress,
+            event.egress,
+            metrics2["observable"],
+            event.depth
+        )
+
+        all_rows.append({
+            f"Local Evening Date @<br> {observatory_1.name}": f"{observatory_1.name}<br>"
+            f"{observing_night_from_sunset(observatory_1, event.mid)}<br>"
+            f"({observatory_1.timezone})",
+            "Moon": (
+                f"{metrics['moon_illum']:.0%} @ "
+                f"{metrics['moon_sep'].to(u.deg).value:.0f}°"
+            ),
+            "Start-Mid-End": (
+                f"{event.ingress.strftime('%H:%M')}<br>"
+                f"{event.mid.strftime('%H:%M')}<br>"
+                f"{event.egress.strftime('%H:%M')}"
+            ),
+            "Alt (S/M/E)": (
+                f"{metrics['alts']['start'].value:.0f}°<br>"
+                f"{metrics['alts']['mid'].value:.0f}°<br>"
+                f"{metrics['alts']['end'].value:.0f}°"
+            ),
+            f"{event.target.name} Transit": icon,
+            
+            "% Transit": 100*metrics['frac_transit'],
+            "% Baseline": 100*metrics['frac_baseline'],
+            f"Local Evening Date @<br> {comparison_observatory.name}": f"{comparison_observatory.name}<br>"
+            f"{observing_night_from_sunset(comparison_observatory, event.mid)}<br>"
+            f"({comparison_observatory.timezone})",
+            f"Transit @ {comparison_observatory.name}": icon2,
+            f"Moon @ {comparison_observatory.name}": (
+                f"{metrics2['moon_illum']:.0%} @ "
+                f"{metrics2['moon_sep'].to(u.deg).value:.0f}°"
+            ),
+            
+            f"% Transit @<br> {comparison_observatory.name}": 100*metrics2['frac_transit'],
+            f"% Baseline @<br> {comparison_observatory.name}": 100*metrics2['frac_baseline'],
+        })
+
+    df = pd.DataFrame(all_rows)
+    if visibility_cut is not None:
+        df = df[df["% Transit"] >= visibility_cut]
+    if plot:
+        return (df.style
+                .format({'% Transit':'{:.0f}%','% Baseline':'{:.0f}%','Depth (ppt)':'{:.2f}',f"% Transit @<br> {comparison_observatory.name}":'{:.0f}%',f"% Baseline @<br> {comparison_observatory.name}":'{:.0f}%',f"% Transit @<br> {comparison_observatory.name}":'{:.0f}%'})
+                .set_properties(**{'background-color':"#FFFFFF",'color':'black',})
+                .set_table_styles([{"selector":"th.col_heading","props":"text-align:center; font-size:1.5em; background-color: #000066;"}])
+                .background_gradient(subset=['% Transit','% Baseline'],cmap="RdYlGn",vmin=0,vmax=100)
+                .background_gradient(subset=[f"% Transit @<br> {comparison_observatory.name}",f"% Baseline @<br> {comparison_observatory.name}"],cmap="RdYlGn",vmin=0,vmax=100))
+    else:
+        return df
